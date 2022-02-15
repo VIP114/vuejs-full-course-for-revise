e77d0b7858b7a3427c82462a56300374666c9652
patelvishv114 <patelvishv343@gmail.com>
2022-02-15 13:09:34 +0530

    10. (1:06:00) get input field value in Vue js

--- src/components/TwoWayBinding9.vue
+++ src/components/TwoWayBinding9.vue
@@ -3,14 +3,30 @@
     <h1>9. (1:01:30) Vue js two way binding</h1>    
     <input type="text" v-model="data">
     <h2>{{data}}</h2>
+    <br/>
+    <input type="text" placeholder="enter email" v-model="email">
+    <br/>
+    <br/>
+    <input type="text" placeholder="Enter password" v-model="password">
+    <br/>
+    <br/>
+    <button type="button" v-on:click="getData()">Get Data</button>
     </div>
 </template>
 <script>
+// import { warn } from '@vue/runtime-core'
 export default {
     name:'TwoWayBinding9',
     data() {
         return {
-            data: ''
+            data: '',
+            email:null,
+            password:null,
+        }
+    },
+    methods: {
+        getData(){
+            console.warn('Emails:-',this.email,'Password:-', this.password);
         }
     },
     
