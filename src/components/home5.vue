8a6b3283d841a9eaff160238eee2c305d44e2a36
patelvishv114 <patelvishv343@gmail.com>
2022-02-15 11:43:18 +0530

    5. (36:40) Make componen in vue js 3

---
+++ src/components/home5.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <h1>Home Component</h1>    
+    </div>
+</template>
+<script>
+export default {
+    name:'Home5'
+    
+}
+</script>
+<style scoped>
+h1{
+    color: red;
+}
+</style>
