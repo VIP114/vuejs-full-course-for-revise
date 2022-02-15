1cbfae2a25b0d9cbe95b12b48657ac055dd09266
patelvishv114 <patelvishv343@gmail.com>
2022-02-15 11:59:33 +0530

    6. (43:35) Vue js Interpolation and data property

--- src/components/home5.vue
+++ src/components/home5.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
     <h1>Home Component</h1>    
+    <h3>Email:- {{email}}</h3>
+    <h3>Moile no:-{{mobile}}</h3>
+    <h3>Moile no:-{{mobile='8734934281'}}</h3>
+    <h3>name: {{getName()}}</h3>
+    <h3>Gender: {{getGender('male')}}</h3>
     </div>
 </template>
 <script>
 export default {
-    name:'Home5'
+    name:'Home5',
+    data() {
+        return {
+            email : 'vishv@gmail.com',
+            mobile:'9558924322',
+            getName(){
+                return 'vishv'
+            },
+            getGender(x){
+                return x
+            }
+        }
+    },
     
 }
 </script>
